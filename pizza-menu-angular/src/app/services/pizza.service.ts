c016df7ea6f5a79f27487af6fbc79a30b93f455a
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-08-08 20:17:31 +0200

    pizzas data to mock file

--- pizza-menu-angular/src/app/services/pizza.service.ts
+++ pizza-menu-angular/src/app/services/pizza.service.ts
@@ -1,54 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Pizza } from '../models/pizza.model';
+import { pizzas } from '../mocks/pizzas-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PizzaService {
-  pizzas: Pizza[] = [
-    {
-      name: 'Focaccia',
-      ingredients: 'Bread with italian olive oil and rosemary',
-      price: 6,
-      photoName: 'pizzas/focaccia.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Margherita',
-      ingredients: 'Tomato and mozarella',
-      price: 10,
-      photoName: 'pizzas/margherita.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Spinaci',
-      ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-      price: 12,
-      photoName: 'pizzas/spinaci.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Funghi',
-      ingredients: 'Tomato, mozarella, mushrooms, and onion',
-      price: 12,
-      photoName: 'pizzas/funghi.jpg',
-      soldOut: false,
-    },
-    {
-      name: 'Pizza Salamino',
-      ingredients: 'Tomato, mozarella, and pepperoni',
-      price: 15,
-      photoName: 'pizzas/salamino.jpg',
-      soldOut: true,
-    },
-    {
-      name: 'Pizza Prosciutto',
-      ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-      price: 18,
-      photoName: 'pizzas/prosciutto.jpg',
-      soldOut: false,
-    },
-  ];
+  private pizzas: Pizza[] = pizzas;
 
   public getPizza(): Pizza[] {
     return this.pizzas;

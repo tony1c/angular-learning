92182d7484e1927c1b51293a8ddb157f431bce0c
Antonio Mario Cavuto <tony@Antonios-MBP.homenet.telecomitalia.it>
2024-08-08 18:06:28 +0200

    added dollar pipe

--- pizza-menu-angular/src/app/components/pizza/pizza.component.ts
+++ pizza-menu-angular/src/app/components/pizza/pizza.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../../services/pizza.service';
 import { Pizza } from '../../models/pizza.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-pizza',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './pizza.component.html',
   styleUrl: './pizza.component.css',
 })
